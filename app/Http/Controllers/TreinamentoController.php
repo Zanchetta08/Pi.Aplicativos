ce7f50761bec28711fcfa5166b84fc517200713a
Zanchetta08 <110983552+Zanchetta08@users.noreply.github.com>
2023-06-12 22:01:37 -0300

    Criação de Treinamentos (Feito)

--- app/Http/Controllers/TreinamentoController.php
+++ app/Http/Controllers/TreinamentoController.php
@@ -18,4 +18,22 @@ class TreinamentoController extends Controller
     public function criar() {
         return view('treinamentos.criar');
     }
+
+    public function store(Request $request) {
+
+        $treinamento = new Treinamento;
+        $treinamento->nome = $request->nome;
+        $treinamento->descricao = $request->desc;
+        $treinamento->cargaHr = $request->cargaHr;
+        $treinamento->inscIni = $request->iniInsc;
+        $treinamento->inscFim = $request->fimInsc;
+        $treinamento->treiIni = $request->iniTrei;
+        $treinamento->treiFim = $request->fimTrei;
+        $treinamento->quantMin = $request->quantMin;
+        $treinamento->quantMax = $request->quantMax;
+
+        $treinamento->save();
+
+        return redirect("/");
+    }
 }
--- resources/views/treinamentos/criar.blade.php
+++ resources/views/treinamentos/criar.blade.php
@@ -3,5 +3,49 @@
 @section('title', 'Criar Treinamento')
 
 @section('content')
+    
+<div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie um treinamento</h1>
+    <form action="/treinamentos" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="nome">Treinamento:</label>
+            <input type="text" class="form-control" id="nomeTrei" name="nome" placeholder="Nome do Treinamento">
+        </div>
+        <div class="form-group">
+            <label for="nome">Descrição:</label>
+            <textarea name="desc" id="descricaoTrei" class="form-control" placeholder="Descrição do Treinamento"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="nome">Carga horária do Treinamento:</label>
+            <input type="number" class="form-control" id="cargaHrTrei" name="cargaHr" min="0">
+        </div>
+        <div class="form-group">
+            <label for="nome">Início das Inscrições:</label>
+            <input type="date" class="form-control" id="inicioInscTrei" name="iniInsc" placeholder="Data do Treinamento">
+        </div>
+        <div class="form-group">
+            <label for="nome">Fim das Inscrições:</label>
+            <input type="date" class="form-control" id="fimInscTrei" name="fimInsc" placeholder="Nome do Treinamento">
+        </div>
+        <div class="form-group">
+            <label for="nome">Início do Treinamento:</label>
+            <input type="date" class="form-control" id="inicioTreiTrei" name="iniTrei" placeholder="Nome do Treinamento">
+        </div>
+        <div class="form-group">
+            <label for="nome">Fim do Treinamento:</label>
+            <input type="date" class="form-control" id="fimTreiTrei" name="fimTrei" placeholder="Nome do Treinamento">
+        </div>
+        <div class="form-group">
+            <label for="nome">Quantidade mínima de participantes:</label>
+            <input type="number" class="form-control" id="quantidadeMinTrei" name="quantMin" min="1">
+        </div>
+        <div class="form-group">
+            <label for="nome">Quantidade máxima de participantes:</label>
+            <input type="number" class="form-control" id="quantidadeMaxTrei" name="quantMax" min="1">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar Treinamento">
+    </form>
+</div>
+
 @endsection
